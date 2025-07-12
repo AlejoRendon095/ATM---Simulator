3822cc291f8b01321e43b3365caa655654188607
AlejoRendon095 <alejandrorendongarzon@gmail.com>
2025-07-11 21:51:59 -0500

    Progress in login and query functionalities

--- main/ATM - Simulator.py
+++ main/ATM - Simulator.py
@@ -1,9 +1,7 @@
 import json
 import os
 
-
-
-def create_account(count):
+def create_account(count, accounts_path):
     name = input("Enter your name: ")
     password = input("Enter your password: ")
     if len(password) < 6:
@@ -13,7 +11,8 @@ def create_account(count):
 
     os.makedirs("src/data", exist_ok=True)
 
-    accounts_path = "src/data/accounts.json"
+    
+
     if os.path.exists(accounts_path):
         with open(accounts_path, "r", encoding="utf-8") as file:
             accounts = json.load(file)
@@ -25,7 +24,7 @@ def create_account(count):
 
 
 
-    with open("src/data/accounts.json",  "w", encoding="utf-8") as file:
+    with open(accounts_path,  "w", encoding="utf-8") as file:
             file.write(json.dumps(accounts, indent=4))
     print(f"Account created successfully! Your account number is {count}.")
     return 1   
@@ -34,18 +33,89 @@ def mainController():
     print("Please select an option:\n 1. Create Account\n 2. Login\n 0. Exit")
     num = int(input("Enter your choice: "))
     return num
+
+def check_balance(accout):
+    print("Your current balance is: ", accout["balance"])    
+
+def deposit():
+    print("soon")
+
+def withdraw():
+    print("soon")
+
+def view_transactions():
+    print("soon")
+
+def logout():
+    print("You have been logged out.\n")
+    return False
+
+def verify_password(account, password):
+    if account["password"] == password:
+        return True
+    else:
+        print("Incorrect password.")
+        return False    
+
+def management_controller(isLoggedIn, accounts):
+    while isLoggedIn:
+        print("\nPlease select an option:\n 1. Check Balance\n 2. Deposit\n 3. Withdraw\n 4. View Transactions\n 5. Logout")
+        choice = input("Enter your choice: ")
+
+        if choice == "1":
+            check_balance(accounts)
+        elif choice == "2":
+            deposit()
+        elif choice == "3":
+            withdraw()
+        elif choice == "4":
+            view_transactions()
+        elif choice == "5":
+            isLoggedIn = logout()
+        else:
+            print("Invalid option. Please try again.")
+
+def login(accounts_path):
+    with open("src/data/accounts.json", "r", encoding="utf-8") as file:
+        accounts = json.load(file)
+
+    for i in range  (accounts.__len__()):
+        print(f"Account {i + 1}: {accounts[str(i + 1)]['name']}")
+
+
+    account_number = input("Enter your account number: ")
+
+    if account_number not in accounts:
+        print("Account not found.")
+        return
     
+    password = input("Enter your password: ")
+
+    if not verify_password(accounts[account_number], password):
+        return
+    
+    print(f"Welcome {accounts[account_number]['name']}!")
+    isLoggedIn = True
+    management_controller(isLoggedIn, accounts[account_number])
+
+
+
+
 
 print("WELCOME TO ATM SIMULATOR")
 isProgramRunning = True
 count = 1
+accounts_path = "src/data/accounts.json"
 while isProgramRunning:
     num = mainController()
     if num == 1:
-        count = count + create_account(count)
+        print("Creating a new account...")
+        count = count + create_account(count, accounts_path)
 
     elif num == 2:
-        print("soon")
+        print("Logging in...")
+        login(accounts_path)
+        
     elif num == 0:
         print("Exiting the ATM Simulator. Goodbye!")
         isProgramRunning = False
